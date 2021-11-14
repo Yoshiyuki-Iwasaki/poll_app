412d8b0cc9972ab551197cbdabface5023ade0f1
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-14 17:49:54 +0900

    投稿機能作成☀️

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Question
 
-admin.site.register(Question)
+from .models import Choice, Question
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
