00c478424552ec99fdb2183f9521f3efed0cfbf0
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-15 17:30:00 +0900

    fix validation

--- polls/views.py
+++ polls/views.py
@@ -32,7 +32,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.",
+            'error_message': "選択が必須です。",
         })
     else:
         selected_choice.votes += 1
